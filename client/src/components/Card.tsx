b3104b5bb3d7d597e6143f45994fe341508a028e
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-07-08 14:11:37 +0000

    frontend card component

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import Typography from "./Typography";
 import Img from "../assets/images/unicornbike.jpg";
 
-const card = () => {
+const Card = () => {
 	return (
-		<div className="max-w-screen-sm max-h-96 m-auto mt-5">
-			<Typography variant={"h6"} color="text-primary-openTitle">
+		<main className="max-w-screen-sm max-h-96 m-auto mt-5">
+			<Typography variant={"h6"} color="text-primary-main">
 				Home Page
 			</Typography>
 			<div className="max-h-96 w-full">
 				<img src={Img} alt="img" className="max-w-full max-h-full" />
 			</div>
-		</div>
+			<div>
+				<Typography>Welcome to the MERN Skeleton home page.</Typography>
+			</div>
+		</main>
 	);
 };
 
-export default card;
+export default Card;
--- client/src/components/Typography.tsx
+++ client/src/components/Typography.tsx
@@ -3,26 +3,32 @@ import { PrimaryColors } from "../Types";
 
 type TypographyOwnProps<T extends React.ElementType> = {
 	variant?: T;
+};
+
+type TextColors = {
 	color?: PrimaryColors | "text-primary-openTitle";
 };
 
-type PropsToOmit<T extends React.ElementType> = keyof TypographyOwnProps<T>;
+type PropsToOmit<
+	T extends React.ElementType,
+	P
+> = keyof (TypographyOwnProps<T> & P);
 
-type TypographyType<T extends React.ElementType> = Omit<
+type TypographyType<T extends React.ElementType, Props = {}> = Omit<
 	React.ComponentPropsWithoutRef<T>,
-	PropsToOmit<T>
+	PropsToOmit<T, Props>
 > &
-	React.PropsWithChildren<TypographyOwnProps<T>>;
+	React.PropsWithChildren<TypographyOwnProps<T> & Props>;
 
 const Typography = <T extends React.ElementType>({
 	variant,
 	children,
 	color,
 	...restProps
-}: TypographyType<T>) => {
+}: TypographyType<T, TextColors>) => {
 	const Component = variant || "h6";
 	return (
-		<Component {...restProps} className={color}>
+		<Component {...restProps} className={`${color}`}>
 			{children}
 		</Component>
 	);
