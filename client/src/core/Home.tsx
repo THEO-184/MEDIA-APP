d913f55a16b434aafbf2111fcc4ec633cb4d9a31
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-07-08 17:05:06 +0000

    endPoints

--- client/src/core/Home.tsx
+++ client/src/core/Home.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UnicornImg from "../assets//images/unicornbike.jpg";
 import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 import Text from "../components/polymorphic";
+import { useFetchUsersQuery } from "../user/api-user";
 
 const Home = () => {
+	const { isLoading, data, isSuccess, error } = useFetchUsersQuery();
+	console.log(data);
 	return (
 		<main>
 			<Card />
