b8bd81638fd558cfa2f8c48d1ad5cd2d2062613d
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-07-10 15:47:27 +0000

    fixed conflict card

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -9,18 +9,8 @@ interface Props {
 
 const Card = (props: Props) => {
 	return (
-<<<<<<< HEAD
 		<main className="w-11/12 p-4 m-auto mt-5 bg-slate-50 rounded-md shadow-md max-h-screen overflow-hidden">
 			<div className="h-14">
-=======
-<<<<<<< HEAD
-		<main className="w-11/12 p-4 m-auto mt-5 bg-slate-50 rounded-md shadow-md max-h-screen overflow-hidden">
-			<div className="h-14">
-=======
-		<main className="w-11/12  m-auto mt-5 bg-slate-50 rounded-md shadow-md max-h-screen overflow-hidden">
-			<div className="h-14 p-4">
->>>>>>> 43e518c66a177dd89cce1bbad8d5e42e471a1ff0
->>>>>>> 94d551e35a4c4ddf2f19fb4e035571271e2f52b2
 				<Typography
 					variant={"h4"}
 					className="font-bold px-1"
@@ -30,10 +20,6 @@ const Card = (props: Props) => {
 					{props.title}
 				</Typography>
 			</div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 94d551e35a4c4ddf2f19fb4e035571271e2f52b2
 			{props.children}
 			{props.description && (
 				<div className="h-14 p-4">
@@ -45,22 +31,6 @@ const Card = (props: Props) => {
 					</Typography>
 				</div>
 			)}
-<<<<<<< HEAD
-=======
-=======
-			<div className="w-full h-[500px]">
-				<img src={Img} alt="img" className="w-full h-full" />
-			</div>
-			<div className="h-14 p-4">
-				<Typography
-					variant={"h6"}
-					className="text-secondary-contrastText font-semibold text-base  px-1"
-				>
-					{props.description}
-				</Typography>
-			</div>
->>>>>>> 43e518c66a177dd89cce1bbad8d5e42e471a1ff0
->>>>>>> 94d551e35a4c4ddf2f19fb4e035571271e2f52b2
 		</main>
 	);
 };
